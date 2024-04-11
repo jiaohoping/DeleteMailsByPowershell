aea9b8e9fefeac4e7135991a8ebb8a3f721c594f
JiaoYang <jiaohoping@163.com>
2024-04-11 13:35:18 +0800

    update

--- DeleteMailsBySome.ps1
+++ DeleteMailsBySome.ps1
@@ -1,46 +1,59 @@
 param(
     [Parameter()]
-    [String]$subject,
-    [String]$searchName
-    
+    [String]$searchName,
+    [String]$searchExpression,
+    [switch]$deleteMail
 )
-Connect-IPPSSession -UserPrincipalName xxxx@xxxx.com
 
-# $Search = New-ComplianceSearch -Name $searchName -ExchangeLocation All  -ContentMatchQuery "Subject:$subject"
-$Search = New-ComplianceSearch -Name $searchName -ExchangeLocation All  -ContentMatchQuery "Subject:$subject"
-Start-ComplianceSearch $Search.Name
+# connect to scc
+$mypwd = ConvertTo-SecureString -String 'the cert password' -Force -AsPlainText
+Connect-IPPSSession -CertificateFilePath "cert file path" -CertificatePassword $mypwd  -AppID "app_id" -Organization "domain_name, such as hesaitech.com" -AzureADAuthorizationEndpointUri https://login.chinacloudapi.cn/common -ConnectionUri https://ps.compliance.protection.partner.outlook.cn/powershell-liveid -ShowBanner:$false
 
+function Delete-MailBySearchName{
+    param ($searchName)
+    Write-Host "start run delete mail function -> " $searchName
+
+    try {
+        New-ComplianceSearchAction -SearchName $searchName -Purge -PurgeType SoftDelete  -ErrorAction Stop
+    }
+    catch {
+        Write-Host "Run delete [" $searchName "] get Exception: " $PSItem.ToString()
+        exit 9001
+    }
+}
+
+
+if ($deleteMail){
+    Delete-MailBySearchName -searchName $searchName
+    exit
+}
+
+Write-Host "start run New-ComplianceSearch -> " $searchName
+
+$Search=New-ComplianceSearch -Name $searchName -ExchangeLocation All -ContentMatchQuery $searchExpression
+write-Host $Search
+
+Start-ComplianceSearch -Identity $Search.Identity
+
+# wait for the search to finish
 while ((Get-ComplianceSearch $Search.Name).Status -ne "Completed") {
     Write-Host " ." -NoNewline
     Start-Sleep -s 3
-    
 }
-Write-Host "Search Done！"
+Write-Host "search done"
+$targetComplianceSearch = Get-ComplianceSearch $Search.Name
+$searchResult = $targetComplianceSearch.SuccessResults
 
-
-$test = Get-ComplianceSearch $Search.Name
-$SearchResultsBySplit = $test.SuccessResults.Trim("{", "}")
-$ResultsHashTable = @{}
-foreach ($item in $SearchResultsBySplit -split "Location:") {
-    $tmp = $item.split(',').Trim()
-    if ($tmp) {
-        $ResultsHashTable[$tmp[0]] = $tmp[1].split(':')[1]
+# filter the search result
+$filteredItems = $searchResult | Select-String -Pattern 'Location:(.*?), Item count: (\d+)' -AllMatches | ForEach-Object {
+    $_.Matches | ForEach-Object {
+        [PSCustomObject]@{
+            Location = $_.Groups[1].Value.Trim()
+            'Item count' = $_.Groups[2].Value.Trim()
+        }
     }
-}
+} | Where-Object { $_.'Item count' -ge 1 }
 
-
-foreach ($exist in $ResultsHashTable.Keys) {
-    if ([Int]$ResultsHashTable[$exist] -gt 0) {
-        Write-Host $exist
-    }
-}
-
-$isDelete = Read-Host "Are u delete  (Y/N): "
-
-if ($isDelete -eq 'Y') {
-    New-ComplianceSearchAction -SearchName $searchName -Purge -PurgeType SoftDelete -Confirm:$false
-}
-
-
-
-# Disconnect-ExchangeOnline
+# write result to json file
+$jsonString = $filteredItems | ConvertTo-Json
+$jsonString | Out-File -FilePath 'output_file.json'
